8da5a30935c88f1527c5831ea4aad68f1693c229
Di Yang <hidiyang@gmail.com>
2013-06-04 09:53:36 +1000

    opencv_gPb update

--- grouping/source/opencv_gpb/src/globalPb.cc
+++ grouping/source/opencv_gpb/src/globalPb.cc
@@ -7,11 +7,8 @@
 //
 
 #include "globalPb.hh"
-#include <assert.h>
-#include <math.h>
 
 using namespace std;
-//using namespace cv;
 
 namespace
 { static double* 
@@ -55,6 +52,96 @@ namespace
 
 namespace cv
 {
+  void
+  pb_parts_final_selected(vector<cv::Mat> & layers
+			  /*
+			  cv::Mat & textons,
+			  vector<cv::Mat> & bg_r3,
+			  vector<cv::Mat> & bg_r5,
+			  vector<cv::Mat> & bg_r10,
+			  vector<cv::Mat> & cga_r5,
+			  vector<cv::Mat> & cga_r10,
+			  vector<cv::Mat> & cga_r20,
+			  vector<cv::Mat> & cgb_r5,
+			  vector<cv::Mat> & cgb_r10,
+			  vector<cv::Mat> & cgb_r20,
+			  vector<cv::Mat> & tg_r5,
+			  vector<cv::Mat> & tg_r10,
+			  vector<cv::Mat> & tg_r20*/)
+  {
+    int n_ori      = 8;                       // number of orientations
+    int num_bins = 25;                        // bins for L, b, a
+    int border     = 30;                      // border pixels
+    double bg_smooth_sigma = 0.1;             // bg histogram smoothing sigma
+    double cg_smooth_sigma = 0.05;            // cg histogram smoothing sigma
+    double sigma_tg_filt_sm = 2.0;            // sigma for small tg filters
+    double sigma_tg_filt_lg = sqrt(2.0)*2.0;  // sigma for large tg filters
+    double tmp_sigma;
+    float ab_min = -73.0, ab_max = 95.0;
+    FILE* pFile_L, *pFile_a, *pFile_b;
+    
+    int tmp_len;
+    int n_bg = 3;
+    int n_cg = 3;
+    int n_tg = 3;
+    int r_bg[3] = { 3,  5, 10 };
+    int r_cg[3] = { 5, 10, 20 };
+    int r_tg[3] = { 5, 10, 20 };
+    
+    cv::Mat color, grey;
+    
+    cv::merge(layers, color);
+    cv::copyMakeBorder(color, color, border, border, border, border, BORDER_REFLECT);
+    cv::cvtColor(color, grey, CV_RGB2GRAY);
+    
+    tmp_sigma = double(num_bins)*bg_smooth_sigma;
+    tmp_len = 2*int(3.0*tmp_sigma+0.5)+1;    
+    cv::Mat bg_smooth_kernel  = cv::getGaussianKernel(tmp_len, tmp_sigma, CV_64FC1);
+    tmp_sigma = double(num_bins)*cg_smooth_sigma;
+    tmp_len = 2*int(3.0*tmp_sigma+0.5)+1;
+    cv::Mat cga_smooth_kernel = cv::getGaussianKernel(tmp_len, tmp_sigma, CV_64FC1);
+    cv::Mat cgb_smooth_kernel = cv::getGaussianKernel(tmp_len, tmp_sigma, CV_64FC1);
+    
+    color.convertTo(color, CV_32FC3);
+    //TODO: normalize color first
+    //ps. It is BGR not RGB
+    cv::pow(color, 2.5, color);
+    cv::cvtColor(color, color, CV_BGR2Lab);
+    pFile_L = fopen("L.txt", "w+");
+    pFile_a = fopen("a.txt", "w+");
+    pFile_b = fopen("b.txt", "w+");
+    cv::split(color, layers);
+    for(size_t c=0; c<3; c++){
+      //cv::minMaxLoc(layers[c], &min_elem, &max_elem);
+      for(size_t i=0; i<layers[c].rows; i++){
+	for(size_t j=0; j<layers[c].cols; j++){
+	  if(c==0)
+	    fprintf(pFile_L, "%0.2f ", layers[c].at<float>(i,j) );
+	  if(c==1)
+	    fprintf(pFile_a, "%0.2f ", layers[c].at<float>(i,j) );
+	  if(c==2)
+	    fprintf(pFile_b, "%0.2f ", layers[c].at<float>(i,j) );
+	}
+	if(c==0)
+	    fprintf(pFile_L, "\n" );
+	  if(c==1)
+	    fprintf(pFile_a, "\n" );
+	  if(c==2)
+	    fprintf(pFile_b, "\n" );
+      }
+    }
+    fclose(pFile_L);
+    fclose(pFile_a);
+    fclose(pFile_b);
+    
+    cv::merge(layers, color);
+    color.convertTo(color, CV_8UC3);
+    
+    cv::imshow("border", color);
+    cv::imshow("layers", layers[0]);
+
+  }
+  
   void 
   MakeFilter(const int radii,
 	     const double theta,
@@ -103,24 +190,18 @@ namespace cv
 
   void
   multiscalePb(const cv::Mat & image,
-	       cv::Mat & bg1)
+	       vector<cv::Mat> & layers)
   {
     double* weights;
-    weigths = _mPb_Weights(image.channels());
-    cv::Mat chR = cv::Mat::zeros(image.rows, image.cols, CV_64FC1);
-    cv::Mat chG = cv::Mat::zeros(image.rows, image.cols, CV_64FC1);
-    cv::Mat chB = cv::Mat::zeros(image.rows, image.cols, CV_64FC1);
-    
-    if(image.channels == 3)
-      cv::split(image, chR, chG, chB);
-    else{
-      cv::clone(image, chR);
-      cv::clone(image, chG);
-      cv::clone(image, chB);
-    }
+    weights = _mPb_Weights(image.channels());
+    layers.resize(3); 
+    if(image.channels() == 3)
+      cv::split(image, layers);
+    else
+      for(size_t i=0; i<3; i++)
+	image.copyTo(layers[i]);
   }
   
-
   void 
   globalPb(const cv::Mat & image,
 	   cv:: Mat & gPb)
@@ -128,8 +209,8 @@ namespace cv
     gPb = cv::Mat::zeros(image.rows, image.cols, CV_64FC3);
     double *weights;
     weights = _gPb_Weights(image.channels());
-    for(size_t i = 0; i < 13; i++)
-      cout<<"weight["<<i<<"]="<<weights[i]<<endl;
+    /*for(size_t i = 0; i < 13; i++)
+      cout<<"weight["<<i<<"]="<<weights[i]<<endl;*/
 
     //multiscalePb - mPb
   
--- grouping/source/opencv_gpb/src/globalPb.hh
+++ grouping/source/opencv_gpb/src/globalPb.hh
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <math.h>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <opencv2/core/core.hpp>
@@ -14,5 +16,26 @@ namespace cv
   MakeFilter(const int radii,
 	     const double theta,
 	     cv::Mat & kernel);
+  
+  void
+  multiscalePb(const cv::Mat & image,
+	       vector<cv::Mat> & layers);
+  
+  void
+  pb_parts_final_selected(vector<cv::Mat> & layers
+			  /*
+			  cv::Mat & textons,
+			  vector<cv::Mat> & bg_r3,
+			  vector<cv::Mat> & bg_r5,
+			  vector<cv::Mat> & bg_r10,
+			  vector<cv::Mat> & cga_r5,
+			  vector<cv::Mat> & cga_r10,
+			  vector<cv::Mat> & cga_r20,
+			  vector<cv::Mat> & cgb_r5,
+			  vector<cv::Mat> & cgb_r10,
+			  vector<cv::Mat> & cgb_r20,
+			  vector<cv::Mat> & tg_r5,
+			  vector<cv::Mat> & tg_r10,
+			  vector<cv::Mat> & tg_r20*/);
 }
 

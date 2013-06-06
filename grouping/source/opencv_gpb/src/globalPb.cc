2e4e9581656f843e54410853c3e85da4b8e5f73e
Di Yang <hidiyang@gmail.com>
2013-06-06 17:29:00 +1000

    update several gaussian filters
    Gaussian 2D filter with varying orientations, dervative orders, and
    sizes.
    
    Gaussian 2D centre-surround filters

--- grouping/source/opencv_gpb/src/globalPb.cc
+++ grouping/source/opencv_gpb/src/globalPb.cc
@@ -9,6 +9,10 @@
 #include "globalPb.hh"
 #define X_ORI 1
 #define Y_ORI 0
+#define ZERO 0
+#define NON_ZERO 1
+#define HILBRT_ON 1
+#define HILBRT_OFF 0
 #define PI 3.141592653
 
 using namespace std;
@@ -52,14 +56,30 @@ namespace
     return weights;
   }
 
+  /********************************************************************************
+   * Distribution Normalize and Mean value shifting
+   ********************************************************************************/
   void
   normalizeDistr(const cv::Mat & input,
-		 cv::Mat & output)
+		 cv::Mat & output,
+		 bool label)
   {
+    bool flag = label ? ZERO : NON_ZERO;
     input.copyTo(output);
     output.convertTo(output, CV_32FC1);
     cv::Mat ones = cv::Mat::ones(output.rows, output.cols, output.type());
     double sumAbs = 0.0;
+    double mean = 0.0;
+    /* If required, zero-mean shift*/
+    if(flag){
+      for(size_t i=0; i<output.rows; i++)
+	for(size_t j=0; j< output.cols; j++)
+	  if(flag)
+	    mean += output.at<float>(i,j);
+      mean = mean/(double(output.rows*output.cols));
+      cv::addWeighted(output, 1.0, ones, -mean, 0.0, output);
+    }
+    /* Distribution Normalized */
     for(size_t i=0; i<output.rows; i++)
       for(size_t j=0; j< output.cols; j++)
 	sumAbs += fabs(output.at<float>(i,j));
@@ -72,25 +92,27 @@ namespace
   void rotate_2D_crop(const cv::Mat & input,
 		      cv::Mat & output,
 		      double ori,
-		      int len_x,
-		      int len_y)
+		      int len_cols,
+		      int len_rows)
   {
     cv::Mat rotate_M = cv::Mat::zeros(2, 3, CV_32FC1);
+    cv::Mat tmp;
     cv::Point center = cv::Point((input.cols-1)/2, (input.rows-1)/2);
-    cv::Size size(len_x, len_y);
     double angle = ori/PI*180.0;
     rotate_M = cv::getRotationMatrix2D(center, angle, 1.0);
     /* Apply rotation transformation to a matrix */
-    cv::warpAffine(input, output, rotate_M, size);
+    cv::warpAffine(input, tmp, rotate_M, input.size());
     
     /* Cropping */
-    
-
+    int border_rows = (input.rows - len_rows)/2;
+    int border_cols = (input.cols - len_cols)/2;
+    cv::Rect cROI(border_cols, border_rows, len_cols, len_rows);
+    output = tmp(cROI);
   }
 
   void rotate_2D(const cv::Mat & input,
-		      cv::Mat & output,
-		      double ori)
+		 cv::Mat & output,
+		 double ori)
   {
     rotate_2D_crop(input, output, ori, input.cols, input.rows);
   }
@@ -125,77 +147,116 @@ namespace
 		    double sigma,
 		    int deriv,
 		    bool hlbrt,
-		    cv::Mat & gaussian)
+		    cv::Mat & output)
   {
+    bool flag = hlbrt? HILBRT_ON : HILBRT_OFF; 
     int len = 2*half_len+1;
     cv::Mat ones = cv::Mat::ones(len, 1, CV_32F);
     double sum_abs;
-    gaussian  = cv::getGaussianKernel(len, sigma, CV_32F);
+    output  = cv::getGaussianKernel(len, sigma, CV_32F);
     if(deriv == 1){
       for(int i=0; i<len; i++){
-	gaussian.at<float>(i) = gaussian.at<float>(i)*double(half_len-i);
+	output.at<float>(i) = output.at<float>(i)*double(half_len-i);
       }
     }
     else if(deriv == 2){
       for(int i=0; i<len; i++){
 	double x = double(i-half_len);
-	gaussian.at<float>(i) = gaussian.at<float>(i)*(x*x/sigma-1.0); 
+	output.at<float>(i) = output.at<float>(i)*(x*x/sigma-1.0); 
       }
     }
-    if(hlbrt){}
-    normalizeDistr(gaussian, gaussian);
+    if(flag){}
+    if(deriv > 0)
+      normalizeDistr(output, output, ZERO);
+    else
+      normalizeDistr(output, output, NON_ZERO);
   }
 
   void 
   gaussianFilter1D(double sigma,
 		   int deriv,
 		   bool hlbrt,
-		   cv::Mat & gaussian)
+		   cv::Mat & output)
   {
     int half_len = int(sigma*3.0);
-    _gaussianFilter1D(half_len, sigma, deriv, hlbrt, gaussian);
+    _gaussianFilter1D(half_len, sigma, deriv, hlbrt, output);
   }
 
-  void 
-  _gaussianFilter2D(double ori,
+  void
+  _gaussianFilter2D(int half_len,
+		    double ori,
 		    double sigma_x,
 		    double sigma_y,
 		    int deriv,
 		    bool hlbrt,
-		    cv::Mat & gaussian)
+		    cv::Mat & output)
   {
-    int half_len_x = int(sigma_x*3.0);
-    int half_len_y = int(sigma_y*3.0);
-    int half_len = (half_len_x>half_len_y)? half_len_x : half_len_y;
+    /* rotate support ROI */
     int len = 2*half_len+1;
-    
     int half_len_rotate_x = support_rotated(half_len, half_len, ori, X_ORI);
     int half_len_rotate_y = support_rotated(half_len, half_len, ori, Y_ORI);
     int half_rotate_len = (half_len_rotate_x > half_len_rotate_y)? half_len_rotate_x : half_len_rotate_y;
-    int len_rotate= 2*half_rotate_len+1;
-    cv::Mat gaussian_x, gaussian_y;
-    
-    /*   Conduct Compution */    
-    _gaussianFilter1D(half_rotate_len, sigma_x, 0,     false, gaussian_x);
-    _gaussianFilter1D(half_rotate_len, sigma_y, deriv, false, gaussian_y);
-    
-    /* debuging patch */
-    /*FILE* pFile1, *pFile2;
-    pFile1 = fopen("p1.txt","w+");
-    pFile2 = fopen("p2.txt","w+");
-    for(size_t i =0; i<gaussian_x.rows; i++){
-      fprintf(pFile1, "%f\n", gaussian_x.at<float>(i,0));
-      fprintf(pFile2, "%f\n", gaussian_y.at<float>(i,0));
-    }
-    fclose(pFile1);
-    fclose(pFile2);*/
+    int len_rotate= 2*half_rotate_len+1;    
+    cv::Mat output_x, output_y;
 
-    cv::transpose(gaussian_y, gaussian_y);
-    gaussian = gaussian_x*gaussian_y;
-    rotate_2D_crop(gaussian, gaussian, ori, len, len);
+    /*   Conduct Compution */    
+    _gaussianFilter1D(half_rotate_len, sigma_x, 0,     HILBRT_OFF, output_x);
+    _gaussianFilter1D(half_rotate_len, sigma_y, deriv, HILBRT_OFF, output_y);
+    output = output_x*output_y.t();
+    rotate_2D_crop(output, output, ori, len, len);
     
     /*  Normalize  */
-    normalizeDistr(gaussian, gaussian);
+    if(deriv > 0)
+      normalizeDistr(output, output, ZERO);
+    else
+      normalizeDistr(output, output, NON_ZERO);
+  }
+
+
+  void 
+  gaussianFilter2D(double ori,
+		   double sigma_x,
+		   double sigma_y,
+		   int deriv,
+		   bool hlbrt,
+		   cv::Mat & output)
+  {
+    bool flag = hlbrt? HILBRT_ON : HILBRT_OFF; 
+    /* actual size of kernel */
+    int half_len_x = int(sigma_x*3.0);
+    int half_len_y = int(sigma_y*3.0);
+    int half_len = (half_len_x>half_len_y)? half_len_x : half_len_y;
+    _gaussianFilter2D(half_len, ori, sigma_x, sigma_y, deriv, hlbrt, output);
+  }
+
+  void
+  _gaussianFilter2D_cs(int half_len,
+		       double ori,
+		       double sigma_x,
+		       double sigma_y,
+		       double scale_factor,
+		       cv::Mat & output)
+  {
+    double sigma_x_c = sigma_x/scale_factor;
+    double sigma_y_c = sigma_y/scale_factor;
+    cv::Mat output_cen, output_sur;
+    _gaussianFilter2D(half_len, ori, sigma_x_c, sigma_y_c, 0, HILBRT_OFF, output_cen);
+    _gaussianFilter2D(half_len, ori, sigma_x,   sigma_y,   0, HILBRT_OFF, output_sur);
+    cv::addWeighted(output_sur, 1.0, output_cen, -1.0, 0.0, output);
+    normalizeDistr(output, output, ZERO);
+  }
+
+  void
+  gaussianFilter2D_cs(double ori,
+		      double sigma_x,
+		      double sigma_y,
+		      double scale_factor,
+		      cv::Mat & output)
+  {
+    int half_len_x = int(sigma_x*3.0);
+    int half_len_y = int(sigma_y*3.0);
+    int half_len = (half_len_x>half_len_y)? half_len_x : half_len_y;    
+    _gaussianFilter2D_cs(half_len, ori, sigma_x, sigma_y, scale_factor, output);
   }
   
   void _texton_Filters(int n_ori,
@@ -231,9 +292,7 @@ namespace cv
     double cg_smooth_sigma = 0.05;            // cg histogram smoothing sigma
     double sigma_tg_filt_sm = 2.0;            // sigma for small tg filters
     double sigma_tg_filt_lg = sqrt(2.0)*2.0;  // sigma for large tg filters
-    double tmp_sigma;
     
-    int tmp_len;
     int n_bg = 3;
     int n_cg = 3;
     int n_tg = 3;
@@ -241,21 +300,17 @@ namespace cv
     int r_cg[3] = { 5, 10, 20 };
     int r_tg[3] = { 5, 10, 20 };
     
-    cv::Mat color, grey, ones;
-    
+    cv::Mat color, grey, ones, bg_smooth_kernel, cga_smooth_kernel, cgb_smooth_kernel;
     cv::merge(layers, color);
     cv::copyMakeBorder(color, color, border, border, border, border, BORDER_REFLECT);
     cv::cvtColor(color, grey, CV_BGR2GRAY);
     ones = cv::Mat::ones(color.rows, color.cols, CV_32FC1);
     
-    tmp_sigma = double(num_bins)*bg_smooth_sigma;
-    tmp_len = 2*int(3.0*tmp_sigma+0.5)+1;    
-    cv::Mat bg_smooth_kernel  = cv::getGaussianKernel(tmp_len, tmp_sigma, CV_32FC1);
-    tmp_sigma = double(num_bins)*cg_smooth_sigma;
-    tmp_len = 2*int(3.0*tmp_sigma+0.5)+1;
-    cv::Mat cga_smooth_kernel = cv::getGaussianKernel(tmp_len, tmp_sigma, CV_32FC1);
-    cv::Mat cgb_smooth_kernel = cv::getGaussianKernel(tmp_len, tmp_sigma, CV_32FC1);
-
+    // Histogram filter generation
+    gaussianFilter1D(double(num_bins)*bg_smooth_sigma, 0, false, bg_smooth_kernel);
+    gaussianFilter1D(double(num_bins)*cg_smooth_sigma, 0, false, cga_smooth_kernel);
+    gaussianFilter1D(double(num_bins)*cg_smooth_sigma, 0, false, cgb_smooth_kernel);
+    
     // Normalize color channels
     color.convertTo(color, CV_32FC3);
     cv::split(color, layers);
@@ -286,11 +341,15 @@ namespace cv
       }
     cv::merge(layers, color);
     cv::imshow("quantized", color);
-
+    
+    /* Test rotated gaussian filter */
     cv::Mat g;
-    _gaussianFilter2D(1.5708, 2.5, 1.0, 2, false, g);    
+    //gaussianFilter2D(1.1781, 2.0, 2.0, 2, HLBRT_OFF, g);
+
+    gaussianFilter2D_cs(0, 2.0, 2.0, M_SQRT2, g);
     FILE* pFile;
     pFile = fopen("gaussian_k.txt","w+");
+    cout<<"writing into gaussian_k.txt ..."<<endl;
     for(size_t i=0; i<g.rows; i++){
       for(size_t j=0; j<g.cols; j++)
 	fprintf(pFile,"%f ", g.at<float>(i,j));
@@ -308,14 +367,14 @@ namespace cv
     double sint, cost, ai, bi;
     double x[5] = {0};
     int wr;
-    cv::Mat A = cv::Mat::zeros(3, 3, CV_64FC1);
-    cv::Mat y = cv::Mat::zeros(3, 1, CV_64FC1);
+    cv::Mat A = cv::Mat::zeros(3, 3, CV_32FC1);
+    cv::Mat y = cv::Mat::zeros(3, 1, CV_32FC1);
     ra = MAX(1.5, double(radii));
     rb = MAX(1.5, double(radii)/4);
     ira2 = 1.0/(pow(ra, 2));
     irb2 = 1.0/(pow(rb, 2));
     wr = int(MAX(ra, rb));
-    kernel = cv::Mat::zeros(2*wr+1, 2*wr+1, CV_64FC3);
+    kernel = cv::Mat::zeros(2*wr+1, 2*wr+1, CV_32FC3);
   
     sint = sin(theta);
     cost = cos(theta);
@@ -329,7 +388,7 @@ namespace cv
       }
     for(size_t i=0; i < 3; i++)
       for(size_t j = i; j < i+3; j++){
-	A.at<double>(i, j-i) = x[j];
+	A.at<float>(i, j-i) = x[j];
       }
     A = A.inv(DECOMP_SVD);
     for(size_t i = 0; i <= 2*wr; i++)
@@ -338,10 +397,10 @@ namespace cv
 	bi =  (double(i)-double(wr))*cost + (double(j)-double(wr))*sint;
 	if((ai*ai*ira2 + bi*bi*irb2) > 1) continue;
 	for(size_t n=0; n < 3; n++)
-	  y.at<double>(n,0) = pow(ai, double(n));
+	  y.at<float>(n,0) = pow(ai, double(n));
 	y = A*y;
 	for(size_t n=0; n < 3; n++)
-	  kernel.at<Vec3d>(j,i)[n] = y.at<double>(n,0);
+	  kernel.at<Vec3d>(j,i)[n] = y.at<float>(n,0);
       }
   }
 
@@ -363,7 +422,7 @@ namespace cv
   globalPb(const cv::Mat & image,
 	   cv:: Mat & gPb)
   {
-    gPb = cv::Mat::zeros(image.rows, image.cols, CV_64FC3);
+    gPb = cv::Mat::zeros(image.rows, image.cols, CV_32FC3);
     double *weights;
     weights = _gPb_Weights(image.channels());
     /*for(size_t i = 0; i < 13; i++)
